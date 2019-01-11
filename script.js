52ad38a5b61b467a85ece4c65dbaf1e20db0f06b
Ivan Platonov <st049378@student.spbu.ru>
2019-01-12 00:19:40 +0300

    Conditionals and Loops

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    message: 'Привет, Vue!'
+    message: "Привет, Vue!"
   }
 })
 
@@ -9,5 +9,23 @@ var app2 = new Vue({
   el: "#app-2",
   data: {
     message: "Вы загрузили эту страницу: " + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true
+  }
+})
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [
+      { text: "Изучить JavaScript" },
+      { text: "Изучить Vue" },
+      { text: "Создать что-нибудь классное" }
+    ]
   }
 })
